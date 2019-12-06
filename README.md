dcac764fe91f7870e0c63e85de7833406fde478f
Spencer <swongsen@ufl.edu>
2019-12-06 14:10:58 -0500

    as

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 UPDATE SIGHTINGS
 SET name = 'California', person= 'Frankfurt', location= 'orlando',sighted = '1923-23-12'
 WHERE (name = 'California flannelbush' and person= 'jay' and location= 'Scodie Mountains' and sighted = '2006-06-26');
+
+
+UPDATE SIGHTINGS
+SET name = 'California', person= 'Frankfurt', location= 'orlando',sighted = '2006-09-29'
+WHERE (name = 'Alpine columbine' and person= 'Jennifer' and location= 'Steve Spring' and sighted = '2006-09-28');
+
+
+SELECT PERSON, LOCATION, SIGHTED FROM SIGHTINGS, FLOWERS WHERE (FLOWERS.COMNAME = "Alpine columbine") ORDER BY SIGHTINGS.SIGHTED DESC LIMIT 10
+
+SELECT * FROM SIGHTINGS ORDER BY SIGHTED DESC
--- assignment5.py
+++ assignment5.py
@@ -6,6 +6,8 @@ from services import monitoring
 
 webclient = Flask(__name__, static_url_path='', static_folder='web_front/static', template_folder='web_front/templates')
 webclient.secret_key = "secretkey"
+flower = "a"
+items = ""
 
 @webclient.route("/")
 def reroute():
@@ -42,28 +44,84 @@ def createUser():
 
 @webclient.route("/home", methods=["GET", "POST"])
 def home():
+    global flower
     # If not logged in, redirect back to login page
     #if not session.get('logged_in'):
     #    return redirect("/login")
     #else:
+    if request.method == "GET":
         conn = sqlite3.connect('flowers2019.db')
         cc = conn.execute('SELECT comname FROM Flowers')
         items = cc.fetchall()
+        conn.close()
         #for row in cc:
         #    print("comname = ", row[0], "\n")
 
         return render_template("home.html", items=items)
 
+    elif request.method == "POST":
+        print("hello")
+        title = request.form["flowername"]
+        flower = title
+        print("title: ", title)
+        print("flower:", flower)
+        return redirect("/flowerinfo")
+
 @webclient.route("/flowerinfo", methods=["GET", "POST"])
 def display():
-    return render_template("flowerinfo.html")
+    global flower
+    global items
+
+    if request.method == "GET":
+        items = ""
+        conn = sqlite3.connect('flowers2019.db')
+        cc = conn.execute('SELECT PERSON, LOCATION, SIGHTED FROM SIGHTINGS, FLOWERS WHERE (FLOWERS.COMNAME = \"'+flower+'\") ORDER BY SIGHTINGS.SIGHTED DESC LIMIT 10' )
+        items = cc.fetchall()
+        print(flower)
+        title = flower
+
+        return render_template("flowerinfo.html", items=items)
 
 @webclient.route("/insert", methods=["GET", "POST"])
 def insert():
-    return render_template("insert.html")
+    global flower
+
+    if request.method == "GET":
+        return render_template("insert.html")
+
+    elif request.method == "POST":
+        return "HELLO"
 
 @webclient.route("/update", methods=["GET", "POST"])
 def update():
-    return render_template("update.html")
+    global flower
+    message = None
+    conn = sqlite3.connect('flowers2019.db')
+
+    if request.method == "GET":
+        return render_template("update.html")
+
+    elif request.method == "POST":
+        message="Updated Successfully"
+        originalperson = request.form["person"]
+        originalflower = request.form["flowername"]
+        originallocation = request.form["location"]
+        originalsighting = request.form["sighted"]
+
+        updateperson = request.form["updatedperson"]
+        updateflower = request.form["updatedflowername"]
+        updatelocation = request.form["updatedlocation"]
+        updatesighting = request.form["updatedsighted"]
+
+        cc = conn.execute('update sightings set name = \'' +updateflower +'\', person= \''+ updateperson+'\', location= \''+updatelocation+'\',sighted = \''+updatesighting
+                           +'\' WHERE (name = \''+originalflower+'\' and person= \''+originalperson+'\' and location= \''+originallocation+'\' and sighted = \''+originalsighting+'\');')
+
+
+        print(originalperson, originalflower, originallocation, originalsighting)
+        print(updateperson, updateflower, updatelocation, updatesighting)
+        return render_template("update.html", message=message)
+
+
+
 if __name__ == "__main__":
     webclient.run()
